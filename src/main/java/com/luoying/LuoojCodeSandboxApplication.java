c0b535a4fbaf42cf87663229991d4ecf61810ef1
1ranxu <263989312@qq.com>
2023-11-12 21:42:26 +0800

    luooj-code-sandbox项目初始化（代码沙箱Java原生实现）

---
+++ src/main/java/com/luoying/LuoojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.luoying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuoojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuoojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luoying/LuoojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuoojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

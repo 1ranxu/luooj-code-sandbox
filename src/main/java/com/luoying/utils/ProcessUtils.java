7140ec4192ad57b22d9aece8f7ea20f6ad92a050
1ranxu <263989312@qq.com>
2023-11-15 20:08:15 +0800

    代码沙箱提供开放 API

--- src/main/java/com/luoying/utils/ProcessUtils.java
+++ src/main/java/com/luoying/utils/ProcessUtils.java
@@ -2,9 +2,12 @@ package com.luoying.utils;
 
 import cn.hutool.core.util.StrUtil;
 import com.luoying.model.ExecuteMessage;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.StopWatch;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 、
@@ -28,30 +31,31 @@ public class ProcessUtils {
             if (exitValue == 0) {
                 System.out.println(opName + "成功");
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                StringBuilder compileOutputStringBuilder = new StringBuilder();
                 String compileOutputLine = "";
+                List<String> outputStrList = new ArrayList<>();
                 while ((compileOutputLine = bufferedReader.readLine()) != null) {
-                    compileOutputStringBuilder.append(compileOutputLine).append("\n");
+                    outputStrList.add(compileOutputLine);
                 }
-                executeMessage.setMessage(compileOutputStringBuilder.toString());
+                executeMessage.setMessage(StringUtils.join(outputStrList,"\n"));
             } else {
                 // 异常退出
                 System.out.println(opName + "失败，错误码：" + exitValue);
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                StringBuilder compileOutputStringBuilder = new StringBuilder();
                 String compileOutputLine = "";
+                List<String> outputStrList = new ArrayList<>();
                 while ((compileOutputLine = bufferedReader.readLine()) != null) {
-                    compileOutputStringBuilder.append(compileOutputLine).append("\n");
+                    outputStrList.add(compileOutputLine);
                 }
-                executeMessage.setMessage(compileOutputStringBuilder.toString());
+                executeMessage.setMessage(StringUtils.join(outputStrList,"\n"));
 
                 BufferedReader errorBufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-                StringBuilder errorCompileOutputStringBuilder = new StringBuilder();
                 String errorCompileOutputLine = "";
+                List<String> erroroutputStrList = new ArrayList<>();
                 while ((errorCompileOutputLine = errorBufferedReader.readLine()) != null) {
-                    errorCompileOutputStringBuilder.append(errorCompileOutputLine).append("\n");
+                    erroroutputStrList.add(errorCompileOutputLine);
                 }
-                executeMessage.setErrorMessage(errorCompileOutputStringBuilder.toString());
+
+                executeMessage.setErrorMessage(StringUtils.join(erroroutputStrList,"\n"));
             }
             watch.stop();
             executeMessage.setTime(watch.getLastTaskTimeMillis());

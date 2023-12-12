f7911e3c082c3772d2ddd15a34c8cf55d4494345
1ranxu <263989312@qq.com>
2023-12-12 22:17:33 +0800

    update:修改代码沙箱模板，解决编译时、运行时异常信息未封装的问题

--- src/main/java/com/luoying/template/CodeSandBoxTemplate.java
+++ src/main/java/com/luoying/template/CodeSandBoxTemplate.java
@@ -2,10 +2,14 @@ package com.luoying.template;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.dfa.FoundWord;
+import cn.hutool.dfa.WordTree;
 import com.luoying.core.CodeSandBox;
 import com.luoying.model.*;
+import com.luoying.model.enums.JudgeInfoMessagenum;
 import com.luoying.utils.ProcessUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
 
 import java.io.File;
 import java.io.IOException;
@@ -30,6 +34,13 @@ public abstract class CodeSandBoxTemplate implements CodeSandBox {
     // 超时时间
     private static final long TIMEOUT = 5000L;
 
+    private static final WordTree WORD_TREE;
+
+    static {
+        WORD_TREE = new WordTree();
+        WORD_TREE.addWords("Files", "exec");
+    }
+
     protected CodeSandBoxTemplate(String globalCodeDirPath, String prefix, String globalCodeFileName) {
         this.globalCodeDirPath = globalCodeDirPath;
         this.prefix = prefix;
@@ -50,13 +61,28 @@ public abstract class CodeSandBoxTemplate implements CodeSandBox {
 
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        StopWatch watch = new StopWatch();
+        watch.start();
         File userCodeFile = null;
         ExecuteCodeResponse executeCodeResponse = null;
+        QuestionSubmitJudgeInfo judgeInfo = null;
         try {
             List<String> inputList = executeCodeRequest.getInputList();
             String code = executeCodeRequest.getCode();
             // 1. 把用户的代码保存为文件
-            userCodeFile = saveCodeToFile(code);
+            try {
+                userCodeFile = saveCodeToFile(code);
+            } catch (Exception e) {
+                watch.stop();
+                judgeInfo = new QuestionSubmitJudgeInfo();
+                judgeInfo.setMessage(JudgeInfoMessagenum.DANGEROUS_OPERATION.getValue());
+                judgeInfo.setTime(watch.getLastTaskTimeMillis());
+
+                return ExecuteCodeResponse.builder()
+                        .message(JudgeInfoMessagenum.DANGEROUS_OPERATION.getValue())
+                        .judgeInfo(judgeInfo)
+                        .build();
+            }
 
             String userCodePath = userCodeFile.getAbsolutePath();
             String userCodeParentPath = userCodeFile.getParentFile().getAbsolutePath();
@@ -66,14 +92,38 @@ public abstract class CodeSandBoxTemplate implements CodeSandBox {
             String runCmd = sandBoxCmd.getRunCmd();
             // 2. 编译代码
             ExecuteMessage compileCodeFileExecuteMessage = compileCode(compileCmd);
-            System.out.println(compileCodeFileExecuteMessage);
+            if (compileCodeFileExecuteMessage.getExitValue() != 0) {
+                log.info("编译信息：{}", compileCodeFileExecuteMessage);
+                watch.stop();
+                judgeInfo = new QuestionSubmitJudgeInfo();
+                judgeInfo.setMessage(JudgeInfoMessagenum.COMPILE_ERROR.getValue());
+                judgeInfo.setTime(watch.getLastTaskTimeMillis());
 
+                return ExecuteCodeResponse.builder()
+                        .message(compileCodeFileExecuteMessage.getErrorMessage())
+                        .judgeInfo(judgeInfo)
+                        .build();
+            }
             // 3. 执行代码，得到输出结果
             List<ExecuteMessage> executeMessageList = runCode(inputList, runCmd);
+            for (ExecuteMessage executeMessage : executeMessageList) {
+                if (executeMessage.getExitValue() != 0) {
+                    watch.stop();
+                    judgeInfo = new QuestionSubmitJudgeInfo();
+                    judgeInfo.setMessage(JudgeInfoMessagenum.RUNTIME_ERROR.getValue());
+                    judgeInfo.setTime(watch.getLastTaskTimeMillis());
+
+                    return ExecuteCodeResponse.builder()
+                            .message(executeMessage.getErrorMessage())
+                            .judgeInfo(judgeInfo)
+                            .build();
+                }
+            }
             // 4. 收集整理输出结果
             executeCodeResponse = getOutputResponse(executeMessageList);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (
+                Exception e) {
+            log.info(e.getMessage());
         } finally {
             // 5. 文件清理，释放空间
             boolean b = deleteFile(userCodeFile);
@@ -93,6 +143,10 @@ public abstract class CodeSandBoxTemplate implements CodeSandBox {
      * @return
      */
     private File saveCodeToFile(String code) {
+        FoundWord foundWord = WORD_TREE.matchWord(code);
+        if (foundWord != null) {
+            throw new RuntimeException("危险操作");
+        }
         // 所有用户代码的根目录
         String userDir = System.getProperty("user.dir");
         String globalCodePath = userDir + File.separator + globalCodeDirPath;
@@ -100,7 +154,7 @@ public abstract class CodeSandBoxTemplate implements CodeSandBox {
             FileUtil.mkdir(globalCodePath);
         }
         // 存放用户代码的具体目录，通过prefix区分不同语言
-        String userCodeParentPath = globalCodePath+ File.separator + prefix + File.separator + UUID.randomUUID();
+        String userCodeParentPath = globalCodePath + File.separator + prefix + File.separator + UUID.randomUUID();
         // 用户代码文件
         String userCodePath = userCodeParentPath + File.separator + globalCodeFileName;
         return FileUtil.writeString(code, userCodePath,
@@ -118,9 +172,6 @@ public abstract class CodeSandBoxTemplate implements CodeSandBox {
         try {
             Process compileProcess = Runtime.getRuntime().exec(compileCmd);
             ExecuteMessage executeMessage = ProcessUtil.runProcessAndGetMessage(compileProcess, "编译");
-            if (executeMessage.getExitValue() != 0) {
-                throw new RuntimeException("编译错误");
-            }
             return executeMessage;
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -205,6 +256,9 @@ public abstract class CodeSandBoxTemplate implements CodeSandBox {
      * @return
      */
     public boolean deleteFile(File userCodeFile) {
+        if (userCodeFile == null) {
+            return true;
+        }
         if (userCodeFile.getParentFile() != null) {
             String userCodeParentPath = userCodeFile.getParentFile().getAbsolutePath();
             boolean del = FileUtil.del(userCodeParentPath);
